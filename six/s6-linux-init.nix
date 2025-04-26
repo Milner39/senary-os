af4a442a8e191b017ac2a6ff2d865e0c177b9792
Adam Joseph <adam@westernsemico.com>
2025-04-26 15:47:41 -0700

    inert(s6-linux-init): formatting

--- six/s6-linux-init.nix
+++ six/s6-linux-init.nix
@@ -177,10 +177,8 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
     mkdir -p $out
-    pushd basedir
-    find . | ${buildPackages.fakeroot}/bin/fakeroot -i $(pwd)/../fakeroot-save-file -- ${buildPackages.cpio}/bin/cpio -Hnewc --create > $out/s6-linux-init.cpio
-    popd
   '' +
+
   # The first thing s6-linux-init does is to mount /run and recursively copy its
   # template into /run/s6-linux-init.  Unfortunately the "template" contains
   # setgid directories and named pipes, which means it the template can't be put
@@ -191,7 +189,12 @@ stdenv.mkDerivation {
   # cpio-unpacking script ahead of s6-linux-init's `init`.  The mount options
   # for /run are the same as those found in s6-linux-init.c.  Statically-linked
   # busybox is used to minimize the size of the closure.
-  #
+  ''
+    pushd basedir
+    find . | ${buildPackages.fakeroot}/bin/fakeroot -i $(pwd)/../fakeroot-save-file -- ${buildPackages.cpio}/bin/cpio -Hnewc --create > $out/s6-linux-init.cpio
+    popd
+  '' +
+
   # six places `init` in $out/boot rather than $out/bin because there is no
   # scenario in which this script should ever be accessible via a user's $PATH.
   ''
