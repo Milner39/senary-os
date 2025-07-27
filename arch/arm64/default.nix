4969768a2650e46d20bd4869eb91bbf0527639aa
Adam Joseph <adam@westernsemico.com>
2025-07-26 20:43:13 -0700

    arch.arm64: remove commented-out boot.loader.update attribute

--- arch/arm64/default.nix
+++ arch/arm64/default.nix
@@ -16,19 +16,4 @@
     params = final.boot.kernel.params;
     dtb    = final.boot.kernel.dtb;
   };
-/*
-  # FIXME this is all wrong
-  boot.loader.update = _:
-    let
-    in pkgs.writeShellScript "update-bootloader" ''
-      ${pkgs.busybox}/bin/busybox blkid | ${pkgs.busybox}/bin/busybox grep -q 'LABEL="${root-device-label}"' || \
-        (echo -e '\n***\nno device with LABEL=${root-device-label}, refusing to update bootloader (sanity check)\n***\n'; exit -1)
-      ${pkgs.busybox}/bin/mkdir -p /run/six/update-bootloader-mountpoint
-      ${pkgs.busybox}/bin/umount /run/six/update-bootloader-mountpoint 2>/dev/null || true # in case it was mounted
-      ${pkgs.busybox}/bin/mount LABEL=${boot-device-label} /run/six/update-bootloader-mountpoint
-      ${pkgs.busybox}/bin/cp -L $2/boot/kernel /run/six/update-bootloader-mountpoint/fallback.uImage
-      ${pkgs.busybox}/bin/cp -L $1/boot/kernel /run/six/update-bootloader-mountpoint/normal.uImage
-      ${pkgs.busybox}/bin/umount /run/six/update-bootloader-mountpoint
-    '';
-*/
 }
