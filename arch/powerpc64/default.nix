8a7b2b9eac68de82e499389fb94a4903d4e70875
Adam Joseph <adam@westernsemico.com>
2025-07-26 21:12:53 -0700

    arch/powerpc64: move petitboot stuff into tags.is-bootloader-petitboot

--- arch/powerpc64/default.nix
+++ arch/powerpc64/default.nix
@@ -10,24 +10,6 @@
   boot.kernel.console.device = _: "hvc0";
   boot.kernel.console.baud.__assign = 115200;
   boot.kernel.payload = _: "${final.boot.kernel.package}/vmlinux";
-  boot.loader.update = _: pkgs.writeShellScript "update-syslinux" ''
-    cat > /boot/syslinux/syslinux.cfg <<EOF
-    DEFAULT sixos-normal
 
-    MENU TITLE ------------------------------------------------------------
-    TIMEOUT 50
-
-    LABEL sixos-normal
-      MENU LABEL sixos
-      LINUX $(readlink -f $1/boot/kernel)
-      INITRD $(readlink -f $1/boot/initrd)
-      APPEND $(cat $1/boot/kernel-params)
-
-    LABEL sixos-fallback
-      MENU LABEL sixos (fallback to last successful boot)
-      LINUX $(readlink -f $2/boot/kernel)
-      INITRD $(readlink -f $2/boot/initrd)
-      APPEND $(cat $2/boot/kernel-params)
-    EOF
-  '';
+  tags.is-bootloader-petitboot.__assign = true;
 }
