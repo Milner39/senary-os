fae8c78470c8ab2f3e2968494a2a6cd3533c24ec
Adam Joseph <adam@westernsemico.com>
2025-10-19 13:32:57 -0700

    services.tor: use execline

--- six/by-name/to/tor/service.nix
+++ six/by-name/to/tor/service.nix
@@ -1,30 +1,32 @@
 { lib
-, six
 , pkgs
+, six
 , targets
 , package ? pkgs.tor
 , conf-file ? throw "conf-file is required"
 , user-name ? throw "user-name is required"
 , group-name ? throw "group-name is required"
+, data-directory
 }:
 
-let
-args = [
-    "-U" "${user-name}:${group-name}"
-    "-u" "${user-name}:${group-name}"
-    "${package}/bin/tor"
-    "-f" "${conf-file}"
-];
-in
 six.mkFunnel {
-  passthru.after = [
-    targets.global.coldplug
-  ];
-  run = pkgs.writeScript "run"
-''
-#!${pkgs.runtimeShell}
-exec 2>&1
-export HOME=/var/lib/tor
-exec ${pkgs.runit}/bin/chpst ${lib.escapeShellArgs args}
-'';
+
+  run =
+    six.util.depot.writeExecline
+      "service.tor.run"
+      { argMode = "none"; }
+      (six.util.execline.seq [
+        [ "${pkgs.busybox}/bin/mkdir" "-p" data-directory ]
+        [ "${pkgs.busybox}/bin/chown" "${user-name}:${group-name}" data-directory ]
+        (six.util.chpst {
+          user = user-name;
+          group = group-name;
+          argv = [
+            "${package}/bin/tor" "-f" "${conf-file}"
+          ];
+        })
+      ]);
+
+  passthru.after = [ targets.global.coldplug ];
+
 }
