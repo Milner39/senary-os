7b428c512adf66f98413f64bf633ca266e6d78ea
Adam Joseph <adam@westernsemico.com>
2025-10-20 11:34:52 -0700

    services.bitcoind: init

---
+++ six/by-name/bi/bitcoind/service.nix
@@ -0,0 +1,51 @@
+{ lib
+, pkgs
+, six
+, targets
+, package ? pkgs.bitcoind
+, user ? "bitcoind"
+, group ? "bitcoind"
+, datadir ? throw "you must specify datadir"
+, extraConfig ? {}
+}:
+
+let
+
+  writeBitcoindConfig = attrs:
+    lib.pipe attrs [
+      (lib.mapAttrsToList (k: v: "${k}=${if lib.isString v then v else toString v}"))
+      (lib.concatStringsSep "\n")
+    ];
+
+  config = {
+    inherit datadir;
+    rpccookiefile = "${datadir}/rpc-cookie";
+    printtoconsole = true;
+    disablewallet = true;
+    nodebuglogfile =true;
+  } // extraConfig;
+
+in
+
+six.mkFunnel {
+
+  run =
+    six.util.depot.writeExecline
+      "service.bitcoind.run"
+      { argMode = "none"; }
+      (six.util.chpst {
+        inherit user;
+        inherit group;
+        argv = [
+          "${package}/bin/bitcoind"
+          "-conf=${pkgs.writeText "bitcoind.conf" (writeBitcoindConfig config)}"
+        ];
+      });
+
+  passthru = {
+    after = [ targets.global.coldplug ];
+    inherit datadir;
+    inherit (config) rpccookiefile;
+  };
+}
+
