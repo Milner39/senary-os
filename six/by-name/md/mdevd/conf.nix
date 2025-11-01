a04fc7ae9faae936e2fb673f3cf12129b9ce68b9
Adam Joseph <adam@westernsemico.com>
2025-11-01 11:20:27 -0700

    six/by-name/md/mdevd/conf.nix: formatting

--- six/by-name/md/mdevd/conf.nix
+++ six/by-name/md/mdevd/conf.nix
@@ -139,12 +139,10 @@ let
       ];
 in
 # Based on the example mdev.conf from mdev-like-a-boss
-pkgs.writeText "mdevd-conf"
-
-  (lib.concatStringsSep "\n" ([
+  lib.pipe ([
 
     # log each event to /run/mdevd-events.log
-    (mkMdevConfLine {
+    {
       stop-if-match = false;
       create-device-node = false;
       devname-regex = ".*";
@@ -158,159 +156,159 @@ pkgs.writeText "mdevd-conf"
           "echo"
         ]}) >> /run/mdevd-events.log"
       ];
-    })
+    }
 
     # support module loading on hotplug
-    (mkMdevConfLine {
+    {
       devname-regex = null;
       env-regexes = { "$MODALIAS" = ".*"; };
       add-argv = [ modprobe-command "\"$MODALIAS\"" ];
       path = null;
-    })
+    }
 
     # /dev/null may already exist; therefore ownership has to be changed with command
-    (mkMdevConfLine {
+    {
       devname-regex = "null";
       octal-mode = "666";
       add-argv = [ busybox "chmod" "666" "$MDEV" ];
-    })
-    (mkMdevConfLine { devname-regex = "zero"; octal-mode = "666"; })
-    (mkMdevConfLine { devname-regex = "full"; octal-mode = "666"; })
-    (mkMdevConfLine { devname-regex = "random"; octal-mode = "444"; })
-    (mkMdevConfLine { devname-regex = "urandom"; octal-mode = "444"; })
-    (mkMdevConfLine { devname-regex = "hwrandom"; octal-mode = "444"; })
-    (mkMdevConfLine { devname-regex = "grsec"; })
+    }
+    { devname-regex = "zero"; octal-mode = "666"; }
+    { devname-regex = "full"; octal-mode = "666"; }
+    { devname-regex = "random"; octal-mode = "444"; }
+    { devname-regex = "urandom"; octal-mode = "444"; }
+    { devname-regex = "hwrandom"; octal-mode = "444"; }
+    { devname-regex = "grsec"; }
 
     # Kernel-based Virtual Machine.
-    (mkMdevConfLine { devname-regex = "kvm"; })
+    { devname-regex = "kvm"; }
 
     # vhost-net, to be used with kvm.
-    (mkMdevConfLine { devname-regex = "vhost-net"; })
+    { devname-regex = "vhost-net"; }
 
-    (mkMdevConfLine { devname-regex = "kmem"; octal-mode = "640"; })
-    (mkMdevConfLine { devname-regex = "mem"; octal-mode = "640"; })
-    (mkMdevConfLine { devname-regex = "port"; octal-mode = "640"; })
+    { devname-regex = "kmem"; octal-mode = "640"; }
+    { devname-regex = "mem"; octal-mode = "640"; }
+    { devname-regex = "port"; octal-mode = "640"; }
     # console may already exist; therefore ownership has to be changed with command
-    (mkMdevConfLine {
+    {
       devname-regex = "console";
       octal-mode = "600";
       add-argv = [ busybox "chmod" "600" "$MDEV" ];
-    })
-    (mkMdevConfLine { devname-regex = "ptmx"; octal-mode = "666"; })
-    (mkMdevConfLine { devname-regex = "pty.*"; })
+    }
+    { devname-regex = "ptmx"; octal-mode = "666"; }
+    { devname-regex = "pty.*"; }
 
     # Typical devices
-    (mkMdevConfLine { devname-regex = "tty"; octal-mode = "666"; })
-    (mkMdevConfLine { devname-regex = "tty[0-9]*"; })
-    (mkMdevConfLine { devname-regex = "vcsa*[0-9]*"; })
-    (mkMdevConfLine { devname-regex = "ttyS[0-9]*"; })
+    { devname-regex = "tty"; octal-mode = "666"; }
+    { devname-regex = "tty[0-9]*"; }
+    { devname-regex = "vcsa*[0-9]*"; }
+    { devname-regex = "ttyS[0-9]*"; }
 
     # block devices
-    (mkMdevConfLine { devname-regex = "ram([0-9]*)"; octal-mode = "660 >rd/%1"; })
-    (mkMdevConfLine { devname-regex = "loop([0-9]+)"; octal-mode = "660 >loop/%1"; })
-    (mkMdevConfLine {
+    { devname-regex = "ram([0-9]*)"; octal-mode = "660 >rd/%1"; }
+    { devname-regex = "loop([0-9]+)"; octal-mode = "660 >loop/%1"; }
+    {
       devname-regex = "sr[0-9]*";
       octal-mode = "660";
       add-argv = [ busybox "ln" "-sf" "$MDEV" "cdrom" ];
-    })
-    (mkMdevConfLine { devname-regex = "fd[0-9]*"; })
-    (mkMdevConfLine {
+    }
+    { devname-regex = "fd[0-9]*"; }
+    {
       env-regexes = { SUBSYSTEM = "block"; };
       octal-mode = "660";
       change-argv = [ "${helpers}/storage-device" ];
-    })
+    }
 
     # Run settle-nics every time new NIC appear.
     # If you don't want to auto-populate /etc/mactab with NICs, run 'settle-nis' without '--write-mactab' param.
     #-SUBSYSTEM=net;DEVPATH=.*/net/.*;.*     root:root 600 @${helpers}/settle-nics --write-mactab
 
-    (mkMdevConfLine { devname-regex = "net/tun[0-9]*"; })
-    (mkMdevConfLine { devname-regex = "net/tap[0-9]*"; octal-mode = "600"; })
+    { devname-regex = "net/tun[0-9]*"; }
+    { devname-regex = "net/tap[0-9]*"; octal-mode = "600"; }
 
   ] ++ lib.optionals alsaSupport [
     # alsa sound devices and audio stuff
-    (mkMdevConfLine {
+    {
       env-regexes = { SUBSYSTEM = "sound"; };
       group = "audio";
       octal-mode = "660";
       add-argv = [ "${helpers}/sound-control" ];
-    })
+    }
   ] ++ [
 
-    (mkMdevConfLine {
+    {
       devname-regex = "adsp";
       group = "audio";
       octal-mode = "660";
       path = "sound/";
       symlink-device-node = true;
-    })
-    (mkMdevConfLine {
+    }
+    {
       devname-regex = "audio";
       group = "audio";
       octal-mode = "660";
       path = "sound/";
       symlink-device-node = true;
-    })
-    (mkMdevConfLine {
+    }
+    {
       devname-regex = "dsp";
       group = "audio";
       octal-mode = "660";
       path = "sound/";
       symlink-device-node = true;
-    })
-    (mkMdevConfLine {
+    }
+    {
       devname-regex = "mixer";
       group = "audio";
       octal-mode = "660";
       path = "sound/";
       symlink-device-node = true;
-    })
-    (mkMdevConfLine {
+    }
+    {
       devname-regex = "sequencer.*";
       group = "audio";
       octal-mode = "660";
       path = "sound/";
       symlink-device-node = true;
-    })
+    }
 
 
     # raid controllers
-    (mkMdevConfLine { devname-regex = "cciss!(.*)"; path = "cciss/%1"; })
-    (mkMdevConfLine { devname-regex = "ida!(.*)"; path = "ida/%1"; })
-    (mkMdevConfLine { devname-regex = "rd!(.*)"; path = "rd/%1"; })
+    { devname-regex = "cciss!(.*)"; path = "cciss/%1"; }
+    { devname-regex = "ida!(.*)"; path = "ida/%1"; }
+    { devname-regex = "rd!(.*)"; path = "rd/%1"; }
 
-    (mkMdevConfLine { devname-regex = "fuse"; octal-mode = "666"; })
+    { devname-regex = "fuse"; octal-mode = "666"; }
 
-    (mkMdevConfLine { devname-regex = "card[0-9]"; group = "video"; path = "dri/"; })
-    (mkMdevConfLine { devname-regex = "dri/.*"; group = "video"; })
+    { devname-regex = "card[0-9]"; group = "video"; path = "dri/"; }
+    { devname-regex = "dri/.*"; group = "video"; }
 
-    (mkMdevConfLine { devname-regex = "agpgart"; path = "misc/"; symlink-device-node = true; })
-    (mkMdevConfLine { devname-regex = "psaux"; path = "misc/"; symlink-device-node = true; })
-    (mkMdevConfLine { devname-regex = "rtc"; octal-mode = "664"; path = "misc/"; symlink-device-node = true; })
+    { devname-regex = "agpgart"; path = "misc/"; symlink-device-node = true; }
+    { devname-regex = "psaux"; path = "misc/"; symlink-device-node = true; }
+    { devname-regex = "rtc"; octal-mode = "664"; path = "misc/"; symlink-device-node = true; }
 
     # input stuff
-    (mkMdevConfLine { devname-regex = "SUBSYSTEM=input;.*"; })
+    { devname-regex = "SUBSYSTEM=input;.*"; }
 
     # v4l stuff
-    (mkMdevConfLine { devname-regex = "vbi[0-9]"; group = "video"; path = "v4l/"; symlink-device-node = true; })
-    (mkMdevConfLine { devname-regex = "video[0-9]"; group = "video"; path = "v4l/"; symlink-device-node = true; })
+    { devname-regex = "vbi[0-9]"; group = "video"; path = "v4l/"; symlink-device-node = true; }
+    { devname-regex = "video[0-9]"; group = "video"; path = "v4l/"; symlink-device-node = true; }
 
     # dvb stuff
-    (mkMdevConfLine { devname-regex = "dvb.*"; group = "video"; })
+    { devname-regex = "dvb.*"; group = "video"; }
 
     # Don't create old usbdev* devices.
-    (mkMdevConfLine { devname-regex = "usbdev[0-9].[0-9]*"; create-device-node = false; })
+    { devname-regex = "usbdev[0-9].[0-9]*"; create-device-node = false; }
 
     # Stop creating x:x:x:x which looks like /dev/dm-*
-    (mkMdevConfLine { devname-regex = "[0-9]+\\:[0-9]+\\:[0-9]+\\:[0-9]+"; create-device-node = false; })
+    { devname-regex = "[0-9]+\\:[0-9]+\\:[0-9]+\\:[0-9]+"; create-device-node = false; }
 
     # /dev/cpu support.
-    (mkMdevConfLine { devname-regex = "microcode"; octal-mode = "600"; path = "cpu/"; })
-    (mkMdevConfLine { devname-regex = "cpu([0-9]+)"; octal-mode = "600"; path = "cpu/%1/cpuid"; })
-    (mkMdevConfLine { devname-regex = "msr([0-9]+)"; octal-mode = "600"; path = "cpu/%1/msr"; })
+    { devname-regex = "microcode"; octal-mode = "600"; path = "cpu/"; }
+    { devname-regex = "cpu([0-9]+)"; octal-mode = "600"; path = "cpu/%1/cpuid"; }
+    { devname-regex = "msr([0-9]+)"; octal-mode = "600"; path = "cpu/%1/msr"; }
 
     # Populate /dev/bus/usb.
-    (mkMdevConfLine {
+    {
       stop-if-match = false;
       env-regexes = {
         SUBSYSTEM = "usb";
@@ -318,10 +316,15 @@ pkgs.writeText "mdevd-conf"
       };
       octal-mode = "660";
       change-argv = [ "${helpers}/dev-bus-usb" ];
-    })
+    }
 
-    ] ++ (lib.map mkMdevConfLine extraStructuredConfig) ++ [
+  ] ++ extraStructuredConfig ++ [
 
     # Catch-all other devices, Right now useful only for debuging.
     #.* root:root 660 *${helpers}/catch-all
-  ]))
+  ]) [
+    (lib.map mkMdevConfLine)
+    (lib.concatStringsSep "\n")
+    (pkgs.writeText "mdevd-conf")
+  ]
+
