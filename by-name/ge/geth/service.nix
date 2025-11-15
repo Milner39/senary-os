afbdfe4c89fc2b81497851ad7d93b242ef083992
Adam Joseph <adam@westernsemico.com>
2025-11-15 11:58:33 -0800

    services.geth: init

---
+++ by-name/ge/geth/service.nix
@@ -0,0 +1,57 @@
+{ lib
+, pkgs
+, six
+, targets
+, package ? pkgs.go-ethereum
+, user ? "geth"
+, group ? "geth"
+, dataDir ? "/var/service/geth"
+, extraConfig ? {}
+}:
+
+let
+
+  defaultConfig = {
+    datadir = dataDir;
+    usb = "false";
+    "authrpc.jwtsecret" = "/run/geth/secret.jwt";
+    "authrpc.addr" = "127.0.0.1";
+    "authrpc.port" = "8551";
+  };
+
+  config = defaultConfig // extraConfig;
+
+in
+six.mkFunnel {
+
+  run =
+    six.util.depot.writeExecline
+      "service.geth.run"
+      { argMode = "none"; }
+      (six.util.execline.seq [
+        [ "${pkgs.busybox}/bin/mkdir" "-m" "0700" "-p" (builtins.dirOf config."authrpc.jwtsecret") ]
+        [ "${pkgs.busybox}/bin/chown" "${user}:${group}" (builtins.dirOf config."authrpc.jwtsecret") ]
+        [ "${pkgs.busybox}/bin/chmod" "0700" (builtins.dirOf config."authrpc.jwtsecret") ]
+        [ "${pkgs.execline}/bin/redirfd" "-w" "1" config."authrpc.jwtsecret"
+          "${pkgs.util-linux}/bin/hexdump" "-n" "32" "-e" "8 \"%08x\" 1 \"\\n\"" "/dev/random" ]
+        (six.util.chpst {
+          inherit user;
+          inherit group;
+          argv = [
+            "${package}/bin/geth"
+          ] ++ lib.pipe config [
+            (lib.mapAttrsToList
+              (key: val:
+                "--${key}=${lib.escapeShellArg (six.lib.toString val)}"))
+          ];
+        })
+      ]);
+
+  passthru = {
+    after = [ targets.global.coldplug ];
+    inherit user group;
+    endpoint = "http://${config."authrpc.addr"}:${config."authrpc.port"}";
+    inherit (config) "authrpc.jwtsecret";
+  };
+}
+
