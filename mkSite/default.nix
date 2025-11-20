e9c87b0226783103de84ba13b6960d38979e7daa
Adam Joseph <adam@westernsemico.com>
2025-11-20 13:11:10 -0800

    mkSite: cleanup

--- mkSite/default.nix
+++ mkSite/default.nix
@@ -15,38 +15,34 @@
 }:
 
 [
-  (site-final: site-prev:
-    #types.site
-    ({
-      inherit (site-dir) subnets overlay globals;
-      tag-overlays =
-        lib.mapAttrs
-          sixos.lib.add-tag-mutation-check-to-overlay
-          tag-overlays;
+  (site-final: site-prev: {
+    inherit (site-dir) subnets globals;
+    tag-overlays =
+      lib.mapAttrs
+        sixos.lib.add-tag-mutation-check-to-overlay
+        tag-overlays;
 
-      # initial host set: populate attrnames from site.hosts
-      hosts =
-        lib.flip lib.mapAttrs site-dir.hosts
-          (name: host-overlay:
-            sixos.mkHost.init {
-              host-final = site-final.hosts.${name};
-              inherit host-overlay;
-              inherit types;
-              inherit name;
-            });
-    }))
+    # initial host set: populate attrnames from site.hosts
+    hosts =
+      lib.flip lib.mapAttrs site-dir.hosts
+        (name: host-overlay:
+          sixos.mkHost.init {
+            host-final = site-final.hosts.${name};
+            inherit host-overlay;
+            inherit types;
+            inherit name;
+          });
+  })
 
 ] ++ map sixos.lib.forall-hosts sixos.mkHost.host-stages ++ [
 
 ] ++ site-dir.overlay ++ [
 
-] ++ lib.map sixos.lib.forall-hosts [
-
-  sixos.mkHost.mkHost
+  (sixos.lib.forall-hosts sixos.mkHost.mkHost)
 
 ] ++ [
 
-  # Add `site` attribute to each host, but do this only after all user
+  # Add the `site` attribute to each host, but do this only after all user
   # overlays so they can't accidentally use `host-prev.site`.
   (site-final: site-prev: site-prev // {
     hosts = lib.mapAttrs (name: host-prev:
