6c18bd9b4a4b8b51fa54664750c6b4d7f8a7d1f4
Adam Joseph <adam@westernsemico.com>
2025-11-30 10:56:05 -0800

    services.syslog: use execlineb instead of runtimeShell

--- by-name/sy/syslog/service.nix
+++ by-name/sy/syslog/service.nix
@@ -3,11 +3,8 @@
 , six
 }:
 six.mkFunnel {
-  run = pkgs.writeScript "run"
-# TO DO: drop privs with `-u`, `-g`, '-U`, `-G`
-''
-#!${pkgs.runtimeShell}
-exec 2>&1
-exec ${pkgs.s6}/bin/s6-socklog -t 60 -x /dev/log
-'';
+  # TO DO: drop privs with `-u`, `-g`, '-U`, `-G`
+  run.argv = [
+    "${pkgs.s6}/bin/s6-socklog" "-t" "60" "-x" "/dev/log"
+  ];
 }
