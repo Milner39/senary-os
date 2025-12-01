3a022faa21ebd5ab9f4c3bb465fdef23a54ae2de
Adam Joseph <adam@westernsemico.com>
2025-12-01 12:41:54 -0800

    mkHost.initHost: cleanup

--- mkHost/initHost.nix
+++ mkHost/initHost.nix
@@ -15,148 +15,6 @@
 
 let
 
-  initialize-attrs =
-    # initial host attrset
-    (host-final: host-prev:
-      host-prev // {
-        inherit (host-prev) name;
-        inherit (host-final) canonical;
-        tags = types.default-tag-values;
-      });
-
-  # set system-isFooBar tags
-  set-system-tags =
-    (host-final: host-prev:
-      host-prev // {
-        tags =
-          types.set-tag-values (host-prev.tags //
-            # This turns each of nixpkgs.lib's predicates "p" into an
-            # attribute "system-${p}" whose value is a boolean
-            # indicating whether or not the predicate matched this
-            # host's `hostPlatform`.  These attribute names will be
-            # intersected with those of site.tags, so if the site
-            # doesn't declare a "system-${p}" tag that's okay.
-            lib.flip lib.mapAttrs' lib.systems.inspect.predicates
-              (predicate-name: predicate-function:
-                let
-                  system = lib.systems.parse.mkSystemFromString host-prev.canonical;
-                  name = "system-${predicate-name}";
-                  value = predicate-function system;
-                in
-                  lib.nameValuePair name value
-              )
-          );
-      });
-
-  gross-hack =
-    # yuck, gross layering violation.  can't fix this until tags are allowed to
-    # (monotonically) modify other tags.
-    (host-final: host-prev:
-      host-prev // {
-        tags =
-          host-prev.tags //
-          lib.optionalAttrs host-prev.tags.system-isAarch64 {
-            has-hwclock-fake = true;
-            has-hwclock = false;
-          } // lib.optionalAttrs host-prev.tags.system-isx86_64 {
-            # x86 machines are required to have a battery-backed RTC
-            has-hwclock-fake = false;
-            has-hwclock = true;
-          } // lib.optionalAttrs host-prev.tags.system-isMips64 {
-            is-bootloader-uboot = true;
-          } // lib.optionalAttrs host-prev.tags.system-isPower64 {
-            # powerpc workstations generally have battery-backed hardware clocks
-            has-hwclock = true;
-            is-bootloader-petitboot = true;
-          };
-      });
-
-  build-ifconns-and-interfaces =
-    # build the ifconns and interfaces attributes
-    (
-      (host-final: host-prev:
-        let
-          ifconns =
-            # all the subnets to which it is directly attached.
-            lib.pipe host-final.site.subnets [
-              (
-                lib.mapAttrs (subnetName: subnet:
-                  lib.pipe subnet [
-                    # drop the __netmask key, which is not a host
-                    (lib.filterAttrs (hostName: _:
-                      !(lib.strings.hasPrefix "__" hostName)
-                    ))
-
-                    # add ${host}.netmask
-                    (lib.mapAttrs
-                      (hostName: ifconn: {
-                        netmask = subnet.__netmask;
-                      } // ifconn))
-                  ])
-              )
-              (lib.mapAttrsToList
-                (subnetName: subnet:
-                  if subnet?${host-prev.name}
-                  then lib.nameValuePair subnetName subnet.${host-prev.name}
-                  else null))
-              (lib.filter (v: v!=null))
-              lib.listToAttrs
-            ];
-        in host-prev // {
-          inherit ifconns;
-          interfaces =
-            { lo.type = "loopback"; } //
-            lib.pipe ifconns [
-              (lib.mapAttrsToList
-                (subnetName: ifconn:
-                  if ifconn?ifname
-                  then lib.nameValuePair ifconn.ifname ({
-                    subnet = subnetName;
-                  } // lib.optionalAttrs (host-final.site.subnets.${subnetName}?__type) {
-                    type = host-final.site.subnets.${subnetName}.__type;
-                  })
-                  else null))
-              (lib.filter (v: v!=null))
-              lib.listToAttrs
-            ];
-        }
-      ));
-
-  kernel-defaults =
-    # default kernel setup
-    (
-      (host-final: host-prev:
-        let
-          mkKernelConsoleBootArg =
-            { device
-            , baud ? null }:
-            "console=${device}"
-            + lib.optionalString (baud!=null) ",${toString baud}";
-        in infuse host-prev {
-          boot.kernel.params   = _: [
-            "root=${host-final.boot.rootfs.parameter}"
-          ] ++ lib.optionals host-final.boot.rootfs.first-mount-is-readonly [
-            "ro"
-          ] ++ lib.optionals (host-final.boot?kernel.console) [
-            (mkKernelConsoleBootArg host-final.boot.kernel.console)
-          ];
-          boot.kernel.modules  = _: "${host-final.boot.kernel.package}";
-          boot.kernel.package  = _: host-final.pkgs.callPackage sixos.mkHost.kernel { };
-          boot.rootfs.label.__assign = "root";
-          boot.rootfs.parameter.__assign = "LABEL=${host-final.boot.rootfs.label}";
-          boot.rootfs.first-mount-is-readonly.__assign = true;
-
-          # If the bootloader or its configuration is stored on a mountable
-          # filesystem, this should be set to that filesystem's LABEL.  Mainly
-          # used for uboot.
-          boot.loader.filesystem.label.__assign = "boot";
-
-          boot.initrd.ttys.__default = { tty0 = null; };
-          boot.initrd.contents.__default = { };
-          boot.kernel.firmware.__default = [];
-        }
-      ));
-
   init = final: prev:
     let
       autoArgs = {
@@ -175,6 +33,10 @@ let
       };
     in
       prev // {
+
+        inherit (prev) name;
+        inherit (final) canonical;
+        tags = types.default-tag-values;
 
         inherit lib yants;
 
@@ -231,90 +93,209 @@ let
       };
 
   initialize-targets =
-    (final: prev: infuse prev [
-      ({
-        targets.default = _: final.six.mkBundle { };
-        targets.global.mounts = _: final.six.mkBundle { passthru.before = [ final.targets.default ]; };
-        targets.global.coldplug = _: final.six.mkBundle { };
-        targets.global.set-hostname = _: final.six.mkBundle { };
-        targets.global.hwclock = _: final.six.mkBundle { };
-        targets.net.iface.__init = lib.pipe final.interfaces [
-          (lib.mapAttrsToList
-            (ifname: interface:
-              if interface.type or null == "loopback"
-              then lib.nameValuePair ifname (final.services.netif {
-                inherit ifname;
-                inherit (interface) type;
-                address = "127.0.0.1";
-                netmask = 8;
-              }) else if interface?subnet
-                 then lib.nameValuePair ifname (
-                   let ifconn = final.ifconns.${interface.subnet};
-                   in if ifconn?wg
-                      then final.services.wireguard ((builtins.removeAttrs ifconn ["ip" "edenPort" "wg"]) // {
-                        inherit ifname;
-                        inherit (ifconn) mtu netmask;
-                        inherit (ifconn.wg) fwmark peers;
-                        private-key-filename = "/etc/wireguard/privatekey";
-                        address = final.ifconns.${interface.subnet}.ip;
-                        listen-port = 201;
-                      })
-                      else final.services.netif ((builtins.removeAttrs ifconn ["ip" "edenPort"]) // {
-                        inherit ifname;
-                      } // lib.optionalAttrs (final.ifconns.${interface.subnet}?ip) {
-                        address = final.ifconns.${interface.subnet}.ip;
-                      }))
-                 else null
-            ))
-          (lib.filter (v: v!=null))
-          (map (lib.flip infuse ({
-            value.__output.passthru.before.__append = [ final.targets.default ];
-          })))
-          lib.listToAttrs
-        ];
-      })
-      ({
-        # TODO: use --onlyonce mounting option?
-        targets.mounts = _: {
-          proc = final.services.mount { where = "/proc"; };
-          sys = final.services.mount { where = "/sys"; };
-          dev.pts = final.services.mount { where = "/dev/pts"; };
-          tmp = final.services.mount {
-            where = "/tmp";
-            fstype = "tmpfs";
-            options = [ "nodev" "nosuid" "nr_inodes=0" "mode=1777" "size=1g" ];
-          };
-          dev.shm = final.services.mount {
-            where = "/dev/shm";
-            options = [ "size=50%" "nosuid" "nodev" "mode=1777" ];
-          };
-          "" = final.services.mount {
-            where = "/";
-            options = [ "remount" "rw" ];
-          };
-        };
-      })
-      {
-        targets = {
-          mdevd.__assign     = final.services.mdevd { };
-          mdevd-coldplug     = _: final.services.mdevd-coldplug { };
-          dnscache           = _: final.services.dnscache { };
-          nix-daemon         = _: final.services.nix-daemon {};
-          # FIXME: logging sshd means it won't start if the root filesystem can't be remounted read-write
-          sshd               = _: final.services.sshd {};
-          syslog             = _: final.services.syslog {};
-          set-hostname       = _: final.services.set-hostname { hostname = final.name; };
+    final: prev: infuse prev {
+      targets.default = _: final.six.mkBundle { };
+      targets.global.mounts = _: final.six.mkBundle { passthru.before = [ final.targets.default ]; };
+      targets.global.coldplug = _: final.six.mkBundle { };
+      targets.global.set-hostname = _: final.six.mkBundle { };
+      targets.global.hwclock = _: final.six.mkBundle { };
+      targets.mdevd.__assign     = final.services.mdevd { };
+      targets.mdevd-coldplug     = _: final.services.mdevd-coldplug { };
+      targets.dnscache           = _: final.services.dnscache { };
+      targets.nix-daemon         = _: final.services.nix-daemon {};
+      # FIXME: logging sshd means it won't start if the root filesystem can't be remounted read-write
+      targets.sshd               = _: final.services.sshd {};
+      targets.syslog             = _: final.services.syslog {};
+      targets.set-hostname       = _: final.services.set-hostname { hostname = final.name; };
+      targets.net.iface.__init = lib.pipe final.interfaces [
+        (lib.mapAttrsToList
+          (ifname: interface:
+            if interface.type or null == "loopback"
+            then lib.nameValuePair ifname (final.services.netif {
+              inherit ifname;
+              inherit (interface) type;
+              address = "127.0.0.1";
+              netmask = 8;
+            }) else if interface?subnet
+               then lib.nameValuePair ifname (
+                 let ifconn = final.ifconns.${interface.subnet};
+                 in if ifconn?wg
+                    then final.services.wireguard ((builtins.removeAttrs ifconn ["ip" "edenPort" "wg"]) // {
+                      inherit ifname;
+                      inherit (ifconn) mtu netmask;
+                      inherit (ifconn.wg) fwmark peers;
+                      private-key-filename = "/etc/wireguard/privatekey";
+                      address = final.ifconns.${interface.subnet}.ip;
+                      listen-port = 201;
+                    })
+                    else final.services.netif ((builtins.removeAttrs ifconn ["ip" "edenPort"]) // {
+                      inherit ifname;
+                    } // lib.optionalAttrs (final.ifconns.${interface.subnet}?ip) {
+                      address = final.ifconns.${interface.subnet}.ip;
+                    }))
+               else null
+          ))
+        (lib.filter (v: v!=null))
+        (map (lib.flip infuse ({
+          value.__output.passthru.before.__append = [ final.targets.default ];
+        })))
+        lib.listToAttrs
+      ];
+    };
 
-          # you need a nixpkgs patch for this
-          #openntpd           = _: final.services.openntpd { };
+  initialize-mounts = final: prev: infuse prev {
+    # TODO: use --onlyonce mounting option?
+    targets.mounts = _: {
+      proc = final.services.mount { where = "/proc"; };
+      sys = final.services.mount { where = "/sys"; };
+      dev.pts = final.services.mount { where = "/dev/pts"; };
+      tmp = final.services.mount {
+        where = "/tmp";
+        fstype = "tmpfs";
+        options = [ "nodev" "nosuid" "nr_inodes=0" "mode=1777" "size=1g" ];
+      };
+      dev.shm = final.services.mount {
+        where = "/dev/shm";
+        options = [ "size=50%" "nosuid" "nodev" "mode=1777" ];
+      };
+      "" = final.services.mount {
+        where = "/";
+        options = [ "remount" "rw" ];
+      };
+    };
+  };
+
+  # set system-isFooBar tags
+  set-system-tags = host-final: host-prev: host-prev // {
+    tags =
+      types.set-tag-values (host-prev.tags //
+                            # This turns each of nixpkgs.lib's predicates "p" into an
+                            # attribute "system-${p}" whose value is a boolean
+                            # indicating whether or not the predicate matched this
+                            # host's `hostPlatform`.  These attribute names will be
+                            # intersected with those of site.tags, so if the site
+                            # doesn't declare a "system-${p}" tag that's okay.
+                            lib.flip lib.mapAttrs' lib.systems.inspect.predicates
+                              (predicate-name: predicate-function:
+                                let
+                                  system = lib.systems.parse.mkSystemFromString host-prev.canonical;
+                                  name = "system-${predicate-name}";
+                                  value = predicate-function system;
+                                in
+                                  lib.nameValuePair name value
+                              )
+      );
+  };
+
+  gross-hack = host-final: host-prev:
+    # yuck, gross layering violation.  can't fix this until tags are allowed to
+    # (monotonically) modify other tags.
+    host-prev // {
+      tags =
+        host-prev.tags //
+        lib.optionalAttrs host-prev.tags.system-isAarch64 {
+          has-hwclock-fake = true;
+          has-hwclock = false;
+        } // lib.optionalAttrs host-prev.tags.system-isx86_64 {
+          # x86 machines are required to have a battery-backed RTC
+          has-hwclock-fake = false;
+          has-hwclock = true;
+        } // lib.optionalAttrs host-prev.tags.system-isMips64 {
+          is-bootloader-uboot = true;
+        } // lib.optionalAttrs host-prev.tags.system-isPower64 {
+          # powerpc workstations generally have battery-backed hardware clocks
+          has-hwclock = true;
+          is-bootloader-petitboot = true;
         };
-      }
-    ]);
+    };
+
+  build-ifconns-and-interfaces =
+    # build the ifconns and interfaces attributes
+    (
+      (host-final: host-prev:
+        let
+          ifconns =
+            # all the subnets to which it is directly attached.
+            lib.pipe host-final.site.subnets [
+              (
+                lib.mapAttrs (subnetName: subnet:
+                  lib.pipe subnet [
+                    # drop the __netmask key, which is not a host
+                    (lib.filterAttrs (hostName: _:
+                      !(lib.strings.hasPrefix "__" hostName)
+                    ))
+
+                    # add ${host}.netmask
+                    (lib.mapAttrs
+                      (hostName: ifconn: {
+                        netmask = subnet.__netmask;
+                      } // ifconn))
+                  ])
+              )
+              (lib.mapAttrsToList
+                (subnetName: subnet:
+                  if subnet?${host-prev.name}
+                  then lib.nameValuePair subnetName subnet.${host-prev.name}
+                  else null))
+              (lib.filter (v: v!=null))
+              lib.listToAttrs
+            ];
+        in host-prev // {
+          inherit ifconns;
+          interfaces =
+            { lo.type = "loopback"; } //
+            lib.pipe ifconns [
+              (lib.mapAttrsToList
+                (subnetName: ifconn:
+                  if ifconn?ifname
+                  then lib.nameValuePair ifconn.ifname ({
+                    subnet = subnetName;
+                  } // lib.optionalAttrs (host-final.site.subnets.${subnetName}?__type) {
+                    type = host-final.site.subnets.${subnetName}.__type;
+                  })
+                  else null))
+              (lib.filter (v: v!=null))
+              lib.listToAttrs
+            ];
+        }
+      ));
+
+  kernel-defaults = host-final: host-prev:
+    # default kernel setup
+    let
+      mkKernelConsoleBootArg =
+        { device
+        , baud ? null }:
+        "console=${device}"
+        + lib.optionalString (baud!=null) ",${toString baud}";
+    in infuse host-prev {
+      boot.kernel.params   = _: [
+        "root=${host-final.boot.rootfs.parameter}"
+      ] ++ lib.optionals host-final.boot.rootfs.first-mount-is-readonly [
+        "ro"
+      ] ++ lib.optionals (host-final.boot?kernel.console) [
+        (mkKernelConsoleBootArg host-final.boot.kernel.console)
+      ];
+      boot.kernel.modules  = _: "${host-final.boot.kernel.package}";
+      boot.kernel.package  = _: host-final.pkgs.callPackage sixos.mkHost.kernel { };
+      boot.rootfs.label.__assign = "root";
+      boot.rootfs.parameter.__assign = "LABEL=${host-final.boot.rootfs.label}";
+      boot.rootfs.first-mount-is-readonly.__assign = true;
+
+      # If the bootloader or its configuration is stored on a mountable
+      # filesystem, this should be set to that filesystem's LABEL.  Mainly
+      # used for uboot.
+      boot.loader.filesystem.label.__assign = "boot";
+
+      boot.initrd.ttys.__default = { tty0 = null; };
+      boot.initrd.contents.__default = { };
+      boot.kernel.firmware.__default = [];
+    };
 
 in [
   init
   initialize-targets
-  initialize-attrs
+  initialize-mounts
   set-system-tags
   gross-hack
   build-ifconns-and-interfaces
