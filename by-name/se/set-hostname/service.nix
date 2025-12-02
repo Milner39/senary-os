86d7d90267f1b99048551cdc758127ffa3e8b58b
Adam Joseph <adam@westernsemico.com>
2025-12-02 10:41:09 -0800

    services.set-hostname: use execlineb instead of runtimeShell

--- by-name/se/set-hostname/service.nix
+++ by-name/se/set-hostname/service.nix
@@ -5,13 +5,12 @@
 , hostname ? throw "missing hostname"
 }:
 six.mkOneshot {
-  # FIXME: this can be done using sysctl, and probably mere writes to /sys
+
   # FIXME: make sure both the hostname and `localhost` are in `/etc/hosts`
-  up = pkgs.writeScript "set-hostname-up.sh" ''
-    #!${pkgs.runtimeShell}
-    exec 2>&1
-    ${pkgs.inetutils}/bin/hostname "${hostname}"
-  '';
+  up = [
+    "${pkgs.busybox}/bin/sysctl"
+    "kernel.hostname=${hostname}"
+  ];
 
   passthru.before = [ targets.global.set-hostname ];
 
