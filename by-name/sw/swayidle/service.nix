df8ed69445c88cd1bd6ed7a78f57e503f5676c01
Adam Joseph <adam@westernsemico.com>
2025-12-29 12:44:59 -0800

    fixes to services.swayidle

--- by-name/sw/swayidle/service.nix
+++ by-name/sw/swayidle/service.nix
@@ -3,26 +3,53 @@
 , six
 , targets
 , sway         ? throw "you must pass swayidle a sway target"
-, lock-command ? "${pkgs.swaylock}/bin/swaylock -e -c 111111"
+
+# swaylock is not a separate s6-rc service for a few reasons: s6-rc is not
+# reentrant (FIXME explain further) and we have no way to recover from `s6-rc
+# start swaylock` failing (due to inability to acquire locks, etc) when called
+# by swayidle.
+, lock-command   ? "${pkgs.swaylock}/bin/swaylock -e -c 111111"
+
 , user         ? sway.passthru.user
 , group        ? sway.passthru.group
 }:
 
-six.mFunnel {
+# TODO: swaylock supports readiness notifications via `--ready-fd` -- useful?
 
-  run = {
-    inherit user group;
-    argv = [
-      "${pkgs.swayidle}/bin/swayidle"
+six.mkFunnel {
 
-      # after 10 minutes, turn off the screens but don't lock the console yet
-      "timeout" "600" (lib.escapeShellArg "swaymsg \"output * power off\"")
-      "resume"        (lib.escapeShellArg "swaymsg \"output * power on\"")
+  inherit user group;
 
-      # after 11 minutes, lock the console
-      "timeout" "660" (lib.escapeShellArg lock-command)
-    ];
-  };
+  env.XDG_RUNTIME_DIR = sway.passthru.xdg-runtime-dir;
+  env.WAYLAND_DISPLAY = "wayland-1";  # FIXME: why?
+
+  # FIXME we need to set this
+  #env.SWAYSOCK = "${sway.passthru.xdg-runtime-dir}/sway-ipc.${user.uid}.${sway-pid}.sock"
+
+  run.argv = [
+    "${pkgs.swayidle}/bin/swayidle"
+
+    # after 10 minutes, turn off the screens but don't lock the console yet
+    "timeout" "600" ''${pkgs.sway}/bin/swaymsg "output * power off"''
+    "resume"        ''${pkgs.sway}/bin/swaymsg "output * power on"''
+
+    # after 11 minutes, lock the console
+    "timeout" "660" lock-command
+  ];
+
+   # swayidle will deparent the swaylock process (unless you use "-w", which
+  # unfortunately causes swayidle to block until swaylock exits), so in order to
+  # cleanly shut down swaylock by sending it SIGUSR1 we need to signal the
+  # entire process group.
+  #
+  finish.argv = [
+    # the `kill` in util-linux lets you send a signal to every process in a
+    # process group; busybox doesn't seem to have this feature.
+    "${pkgs.util-linux}/bin/kill"
+    "-USR1"
+    "--"
+    "-\${4}"  # the fourth argument to a finish script is the process group
+  ];
 
   passthru.after = [ sway ];
 }
