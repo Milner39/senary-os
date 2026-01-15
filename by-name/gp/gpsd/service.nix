44dc721a6645fff408d3b6ae0563555953bff325
Adam Joseph <adam@westernsemico.com>
2026-01-14 19:13:48 -0800

    services.gpsd: use execlineb instead of pkgs.runtimeShell

--- by-name/gp/gpsd/service.nix
+++ by-name/gp/gpsd/service.nix
@@ -21,33 +21,26 @@ let
     gpsdGroup = group;
   };
 
-  options = [
-    "-b"                        # bluetooth-safe: open data sources read-only
-    "-n"                        # don't wait for client connects to poll GPS
-    "-N"                        # don't go into background
-    "-F" gpsd-socket            # specify control socket location
-    "-G"                        # make gpsd listen on INADDR_ANY
-    "-D" debug-level            # set debug level
-    "-S" (toString daemon-port) # set port for daemon
-    gps-device
-  ];
-
-  run = pkgs.writeScript "run" ''
-    #!${pkgs.runtimeShell}
-    exec 2>&1
-    chown ${user}:${group} ${gps-device}
-    #rm -rf /var/run/gpsd/
-    mkdir -p /var/run/gpsd/
-    mkdir -p /run/gpsd/
-    chown ${user}:${group} /var/run/gpsd/
-    chown ${user}:${group} /run/gpsd/
-    exec ${package}/bin/gpsd ${lib.escapeShellArgs options}
-  '';
-
 in six.mkFunnel {
   passthru = passthru // {
     after = (passthru.after or []) ++ [ targets.global.coldplug ];
     inherit gpsd-socket user group;
   };
-  inherit run;
+
+  mkdir = {
+    "/var/run/gpsd/" = "0644";
+    "/run/gpsd/"     = "0644";
+  };
+
+  run.argv = [
+    "${package}/bin/gpsd"
+    "-b"                        # bluetooth-safe: open data sources read-only
+    "-n"                        # don't wait for client connects to poll GPS
+    "-N"                        # don't go into background
+    "-F" gpsd-socket            # specify control socket location
+    "-G"                        # make gpsd listen on INADDR_ANY
+    "-D" (toString debug-level) # set debug level
+    "-S" (toString daemon-port) # set port for daemon
+    gps-device
+  ];
 }
