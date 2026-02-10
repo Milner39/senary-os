8b0a6ec91fafc73dc71e0c8885c163c0516a7915
Adam Joseph <adam@westernsemico.com>
2026-02-09 22:01:59 -0800

    services.pcscd: cleanup, better options

--- by-name/pc/pcscd/service.nix
+++ by-name/pc/pcscd/service.nix
@@ -2,24 +2,37 @@
 , pkgs
 , six
 , targets
+, extra-args ? []
+, driver-paths ? []
 }:
 
 let
+  plugins = pkgs.buildEnv {
+    name = "pcscd-plugins";
+    paths = [ "${pkgs.ccid}/pcsc/drivers" ] ++ driver-paths;
+  };
+
   env = {
-    PCSCLITE_HP_DROPDIR = "${pkgs.ccid}/pcsc/drivers";
+    PCSCLITE_HP_DROPDIR = "${plugins}";
   };
 
   run.argv = [
+
     "${pkgs.pcsclite}/bin/pcscd"
+
     "-f"  # foreground
+
     # bug causes mass-scanning of /etc if no configuration file
     # is provided; see https://github.com/NixOS/nixpkgs/issues/121088
     "-c" "/dev/null"
-    #"--debug"
-    "-a"  # log APDU commands+results
-  ];
+
+    "--disable-polkit"
+
+  ] ++ extra-args;
 
 in six.mkFunnel {
   inherit env run;
-  passthru.after = with targets; [ global.coldplug ];
+  passthru.after = [
+    targets.global.coldplug
+  ];
 }
