b036ad39c20b53727fd5323a6e1171762578f673
Adam Joseph <adam@westernsemico.com>
2026-04-06 10:51:12 -0700

    mkHost/initrd.nix: create critical mountpoints if they do not exist, before switch_root

--- mkHost/initrd.nix
+++ mkHost/initrd.nix
@@ -134,6 +134,31 @@ let
     ''];
   }));
 
+  #
+  # Mountpoints for /dev /sys /run and /proc must exist before we switch_root to
+  # an s6-linux-init-based root filesystem.  If they don't exist (typically on
+  # the very first boot after a new install), we take the path of least evil and
+  # try to create them by remounting read-write.
+  #
+  ensure-mountpoints =
+  (final: prev: let
+    inherit (final) pkgs;
+  in infuse prev ({
+    boot.initrd.contents."early/finish".__append = [''
+      if ! (test -e /root/dev && test -e /root/sys && test -e /root/run && test -e /root/proc); then
+        echo "root filesystem is missing one or more of /{dev,sys,run,proc}; remounting read-write to create them..."
+        set -x
+        mount -o remount,rw /root
+        mkdir -m 0000 -p /root/dev
+        mkdir -m 0000 -p /root/run
+        mkdir -m 0000 -p /root/sys
+        mkdir -m 0000 -p /root/proc
+        mount -o remount,ro /root
+        set +x
+      fi
+    ''];
+  }));
+
   # switch_root into the chosen profile
   switch-root =
   (final: prev: let
@@ -171,6 +196,7 @@ in [
   minimal-contents
   cryptsetup-initrd
   lvm-initrd
+  ensure-mountpoints
   switch-root
   mount-root
 ]
