56980220ee7537b01be0c89c42111a0c044e40cf
Adam Joseph <adam@westernsemico.com>
2026-04-17 09:39:15 -0700

    mkConfiguration: create /etc if it does not exist

--- mkConfiguration/default.nix
+++ mkConfiguration/default.nix
@@ -308,6 +308,7 @@ let
 
         ${pkgs.busybox}/bin/mount --bind / $RWMOUNT
         ${pkgs.busybox}/bin/mount -o remount,rw / $RWMOUNT
+        ${pkgs.busybox}/bin/mkdir -m 0555 -p $RWMOUNT/etc
         ${pkgs.busybox}/bin/mkdir -m 0555 -p $RWMOUNT/bin
         ${pkgs.busybox}/bin/mkdir -m 0555 -p $RWMOUNT/usr/bin
         ${linkify "/bin/sh"}
