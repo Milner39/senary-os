09d66d04f33d95050ea3cfcbc7fcd40e4aa8cd9c
Adam Joseph <adam@westernsemico.com>
2026-05-19 16:04:57 -0700

    tags.is-bootloader-grub: copy kernel and initrd to ${final.boot.loader.filesystem.label}

--- tags/is-bootloader-grub/overlays.nix
+++ tags/is-bootloader-grub/overlays.nix
@@ -3,32 +3,32 @@
 , ...
 }:
 
-[(final: prev: infuse prev {
+[
+  (final: prev: infuse prev {
+    boot.loader.filesystem.spec.__default = null;
+  })
 
-/*
-  # TODO(amjoseph): get rid of `*.ubootenv` (it has serious shell-quoting
-  # issues) and instead run `mkimage` from inside the `update-bootloader`
-  # script.  We can't hardwire the configuration outpath into the `uImage`
-  # because they're built by separate derivations and this would create a
-  # reference cycle in `/nix/store`.  This will also clean up the install-media
-  # situation.
-  boot.loader.update.__assign =
+  # TODO: add the ability to (optionally) copy the kernel and initrd to
+  # specified paths on a specified device at update-bootloader time.
+  #
+  # Really, though, this logic ought to be shared with uboot and petitboot.
+
+  (final: prev: infuse prev {
+    boot.loader.update.__assign =
+      if final.boot.loader.filesystem.spec == null
+      then null
+      else
     final.pkgs.writeShellScript "update-bootloader" ''
-      ${final.pkgs.busybox}/bin/busybox blkid | ${final.pkgs.busybox}/bin/busybox grep -q 'LABEL="${final.boot.loader.filesystem.label}"' || \
-        (echo -e '\n***\nno device with LABEL=${final.boot.loader.filesystem.label}, refusing to update bootloader (sanity check)\n***\n'; exit -1)
       ${final.pkgs.busybox}/bin/mkdir -p /run/six/update-bootloader-mountpoint
       ${final.pkgs.busybox}/bin/umount /run/six/update-bootloader-mountpoint 2>/dev/null || true # in case it was mounted
-      ${final.pkgs.busybox}/bin/mount LABEL=${final.boot.loader.filesystem.label} /run/six/update-bootloader-mountpoint
-      ${final.pkgs.busybox}/bin/cp -L $2/boot/kernel         /run/six/update-bootloader-mountpoint/fallback.uImage
-      ${final.pkgs.busybox}/bin/echo -n bootargs=         >  /run/six/update-bootloader-mountpoint/fallback.ubootenv
-      ${final.pkgs.busybox}/bin/cat $2/boot/kernel-params >> /run/six/update-bootloader-mountpoint/fallback.ubootenv
-      ${final.pkgs.busybox}/bin/echo                      >> /run/six/update-bootloader-mountpoint/fallback.ubootenv
-      ${final.pkgs.busybox}/bin/cp -L $1/boot/kernel         /run/six/update-bootloader-mountpoint/normal.uImage
-      ${final.pkgs.busybox}/bin/echo -n bootargs=         >  /run/six/update-bootloader-mountpoint/normal.ubootenv
-      ${final.pkgs.busybox}/bin/cat $1/boot/kernel-params >> /run/six/update-bootloader-mountpoint/normal.ubootenv
-      ${final.pkgs.busybox}/bin/echo                      >> /run/six/update-bootloader-mountpoint/normal.ubootenv
+      ${final.pkgs.busybox}/bin/mount ${final.boot.loader.filesystem.spec} /run/six/update-bootloader-mountpoint || exit -1
+      ${final.pkgs.busybox}/bin/mkdir -p /run/six/update-bootloader-mountpoint/sixos/fallback
+      ${final.pkgs.busybox}/bin/mkdir -p /run/six/update-bootloader-mountpoint/sixos/normal
+      ${final.pkgs.busybox}/bin/cp -L $2/boot/kernel         /run/six/update-bootloader-mountpoint/sixos/fallback/kernel
+      ${final.pkgs.busybox}/bin/cp -L $2/boot/initrd         /run/six/update-bootloader-mountpoint/sixos/fallback/initrd
+      ${final.pkgs.busybox}/bin/cp -L $1/boot/kernel         /run/six/update-bootloader-mountpoint/sixos/normal/kernel
+      ${final.pkgs.busybox}/bin/cp -L $1/boot/initrd         /run/six/update-bootloader-mountpoint/sixos/normal/initrd
       ${final.pkgs.busybox}/bin/umount /run/six/update-bootloader-mountpoint
-    ''
-"";
-*/
-})]
+    '';
+})
+]
