13ce1994057686fc9386a1511b8e0c08fb58a35b
Adam Joseph <adam@westernsemico.com>
2026-06-16 17:49:17 -0700

    default.nix: bump infuse dependency

--- default.nix
+++ default.nix
@@ -11,8 +11,8 @@
   infuse
   ? ((import (builtins.fetchGit {
     url = "https://codeberg.org/amjoseph/infuse.nix";
-    rev = "73c5111fdb7c0faab55bd9a19b26821639a4258e";
-    shallow = true;
+    ref = "v2.5";
+    rev = "d3f4e49112f9a59e701ac067faec6832149df07c";
   })) { inherit lib; }).v1.infuse,
 
   nixpkgs
